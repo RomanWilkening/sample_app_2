59c216215002ac142657e58551e8bf24e6ca3225
Roman Wilkening <rwilkening@online.de>
2015-01-13 15:42:01 +0100

    Init SA2

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  include SessionsHelper
+
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+    super
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+end
